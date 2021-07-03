10e8f5cce70eb4ba60d529d09c07f19f965d5f8b
Marcel van Wyk <vanwyknbake@gmail.com>
2021-07-03 17:50:55 +0200

    '..'

---
+++ blog/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+from sorl.thumbnail import ImageField
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, blank=True)
+    content = models.TextField()
+    image = models.FileField(blank=True, upload_to='postpics')
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
+    
+
+
+
+
+
+
+    
